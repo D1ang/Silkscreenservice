c641558fb1f1a17c5371c6f234e73c14ce3e9493
Djang <django.heimgartner@gmail.com>
2020-06-05 15:41:27 +0200

    Created model admin lists & reorganised the models

--- orders/admin.py
+++ orders/admin.py
@@ -2,15 +2,41 @@ from django.contrib import admin
 from .models import Item, ItemTag, OrderItem, Order
 
 
+class ItemTagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'colour'
+    )
+
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = (
         'name',
+        'tag',
         'price',
         'clicks'
     )
 
 
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'item',
+        'user',
+        'quantity',
+        'ordered'
+    )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'date',
+        'user',
+        'status',
+        'ordered'
+    )
+
+
 admin.site.register(Item, ItemAdmin)
-admin.site.register(ItemTag)
-admin.site.register(OrderItem)
-admin.site.register(Order)
+admin.site.register(ItemTag, ItemTagAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(Order, OrderAdmin)
--- orders/models.py
+++ orders/models.py
@@ -3,22 +3,19 @@ from django.db import models
 from django.shortcuts import reverse
 
 
-STATUS = (
-    ('requested', 'Requested'),
-    ('pending', 'Pending'),
-    ('finished', 'Finished')
-)
-
-TAG_COLOURS = (
-    ('danger', 'Red'),
-    ('success', 'Green'),
-    ('info', 'Blue'),
-    ('warning', 'Yellow'),
-    ('secondary', 'Gray')
-)
-
-
 class ItemTag(models.Model):
+    """
+    A model for the item tags with selectable
+    Bootstrap colours.
+    """
+    TAG_COLOURS = (
+        ('danger', 'Red'),
+        ('success', 'Green'),
+        ('info', 'Blue'),
+        ('warning', 'Yellow'),
+        ('secondary', 'Gray')
+    )
+
     name = models.CharField(max_length=10)
     colour = models.CharField(choices=TAG_COLOURS,
                               max_length=10, default='Red')
@@ -28,6 +25,10 @@ class ItemTag(models.Model):
 
 
 class Item(models.Model):
+    """
+    A model for the items/products.
+    Urls are slug based.
+    """
     name = models.CharField(max_length=20)
     slug = models.SlugField()
     description = models.TextField(max_length=70)
@@ -42,38 +43,48 @@ class Item(models.Model):
             'slug': self.slug
         })
 
-    def __str__(self):
-        return self.name
-
-    
-
     def get_add_to_cart_url(self):
         return reverse('orders:add_to_cart', kwargs={
             'slug': self.slug
         })
 
+    def __str__(self):
+        return self.name
+
 
 class OrderItem(models.Model):
-    user = models.ForeignKey(
-          settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    """
+    A model for the order items
+    to be collected by the order.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    ordered = models.BooleanField(default=False)
-    # QUANTITY IS NOT NEEDED!!!!
     quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
-        return '{0} @ {1}'.format(self.item.name,
-                                  self.item.price)
+        return self.item.name
 
 
 class Order(models.Model):
+    """
+    A model to collect
+    the order items in 1 order
+    """
+    STATUS = (
+        ('requested', 'Requested'),
+        ('pending', 'Pending'),
+        ('finished', 'Finished')
+    )
+
+    date = models.DateField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     status = models.CharField(
         choices=STATUS, default='requested', max_length=10)
-    ordered = models.BooleanField(default=False)
     items = models.ManyToManyField(OrderItem)
-    date = models.DateField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
