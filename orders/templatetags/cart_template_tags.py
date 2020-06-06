d3b050b43da237150ced4674214dfbf05d545e5f
Djang <django.heimgartner@gmail.com>
2020-06-06 14:59:31 +0200

    Created a counter for the cart icon in  the navbar

---
+++ orders/templatetags/cart_template_tags.py
@@ -0,0 +1,13 @@
+from django import template
+from orders.models import Order
+
+register = template.Library()
+
+
+@register.filter
+def cart_item_count(user):
+    if user.is_authenticated:
+        query = Order.objects.filter(user=user, ordered=False)
+        if query.exists():
+            return query[0].items.count()
+    return 0
