bcfa859daf02875717c4b04aa3a0e6fe04b0f568
Djang <django.heimgartner@gmail.com>
2020-06-06 09:46:36 +0200

    Created custom style for signup.html with CSS & JS

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  /* Finds all the inputfields of the account signup form
+     & adds proper Bootstrap class styling
+     code is based on the following: https://stackoverflow.com/a/41909370 */
+
+	$('#signupForm').find(':input').each(function(index, element) {
+		$(element).addClass('form-control');
+	});
+});
