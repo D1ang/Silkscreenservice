9412886105702f46b8b11e90772629733dfe2299
Djang <django.heimgartner@gmail.com>
2020-06-06 13:08:06 +0200

    Set style & animations for all the Django alerts

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-
-  /* Finds all the inputfields of the account signup form
+	/* Finds all the inputfields of the account signup form
      & adds proper Bootstrap class styling
      code is based on the following: https://stackoverflow.com/a/41909370 */
 
 	$('#auth-form').find(':input').each(function(index, element) {
 		$(element).addClass('form-control');
+	});
+
+	// show alerts with animation & hide them after showing.
+	$('.alert').first().hide().slideDown(500).delay(4000).slideUp(500, function() {
+		$(this).remove();
 	});
 });
