b86db8458e780ec7062f144c65006819682b4189
Djang <django.heimgartner@gmail.com>
2020-06-10 12:28:39 +0200

    Set & styled the stripe card error messages

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,6 +1,7 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
+
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
@@ -9,12 +10,13 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
+
 var style = {
 	base: {
 		color: '#32325d',
-		fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontFamily: '"Poppins", sans-serif',
 		fontSmoothing: 'antialiased',
-		fontSize: '16px',
+		fontSize: '17px',
 		'::placeholder': {
 			color: '#aab7c4'
 		}
@@ -29,12 +31,17 @@ var card = elements.create('card', { style: style });
 card.mount('#card-element');
 
 // Handle real-time validation errors from the card Element.
-card.on('change', function(event) {
-	var displayError = document.getElementById('card-errors');
+card.addEventListener('change', function(event) {
+	var errorDiv = document.getElementById('card-errors');
 	if (event.error) {
-		displayError.textContent = event.error.message;
+    var html = `
+      <div class="stripe-icon my-2 pl-1">
+        <i class = "fas fa-times"></i> ${event.error.message}
+      </div>
+    `;
+		$(errorDiv).html(html);
 	} else {
-		displayError.textContent = '';
+		errorDiv.textContent = '';
 	}
 });
 
