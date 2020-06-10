bb1d321a3eda8f8a35321921347c055b7d8dbe2d
Djang <django.heimgartner@gmail.com>
2020-06-10 10:56:36 +0200

    Replaced old test code with cleaner Stripe V3 code

---
+++ static/js/stripe.js
@@ -0,0 +1,70 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+	base: {
+		color: '#32325d',
+		fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+		fontSmoothing: 'antialiased',
+		fontSize: '16px',
+		'::placeholder': {
+			color: '#aab7c4'
+		}
+	},
+	invalid: {
+		color: '#fa755a',
+		iconColor: '#fa755a'
+	}
+};
+
+var card = elements.create('card', { style: style });
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.on('change', function(event) {
+	var displayError = document.getElementById('card-errors');
+	if (event.error) {
+		displayError.textContent = event.error.message;
+	} else {
+		displayError.textContent = '';
+	}
+});
+
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	stripe.createToken(card).then(function(result) {
+		if (result.error) {
+			// Inform the user if there was an error.
+			var errorElement = document.getElementById('card-errors');
+			errorElement.textContent = result.error.message;
+		} else {
+			// Send the token to your server.
+			stripeTokenHandler(result.token);
+		}
+	});
+});
+
+// Submit the form with the token ID.
+function stripeTokenHandler(token) {
+	// Insert the token ID into the form so it gets submitted to the server
+	var form = document.getElementById('payment-form');
+	var hiddenInput = document.createElement('input');
+	hiddenInput.setAttribute('type', 'hidden');
+	hiddenInput.setAttribute('name', 'stripeToken');
+	hiddenInput.setAttribute('value', token.id);
+	form.appendChild(hiddenInput);
+
+	// Submit the form
+	form.submit();
+}
