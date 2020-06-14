05834b85bdb8bc08138c1a44b2238f3c1b8186f9
Djang <django.heimgartner@gmail.com>
2020-06-14 15:51:12 +0200

    Bugfix & improved navbar, made it react on scroll

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   //DataTable settings
-  var table = $('#dataTable').DataTable({
+  let table = $('#dataTable').DataTable({
     lengthChange: false,
     dom: 'lrtip',
     info: false,
@@ -18,7 +18,6 @@ $(document).ready(function() {
 	/* Finds all the inputfields of the account signup form
      & adds proper Bootstrap class styling
      code is based on the following: https://stackoverflow.com/a/41909370 */
-
 	$('#auth-form').find(':input').each(function(index, element) {
 		$(element).addClass('form-control');
 	});
@@ -26,5 +25,19 @@ $(document).ready(function() {
 	// show alerts with animation & hide them after showing.
 	$('.alert').first().hide().slideDown(500).delay(4000).slideUp(500, function() {
 		$(this).remove();
-	});
+  });
+
+	/* When the user scrolls down, hide the navbar.
+     When the user scrolls up, show the navbar
+     https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp */
+     let prevScrollpos = window.pageYOffset;
+     window.onscroll = function() {
+       let currentScrollPos = window.pageYOffset;
+       if (prevScrollpos > currentScrollPos) {
+         document.getElementById('navbar').style.top = '0';
+       } else {
+         document.getElementById('navbar').style.top = '-60px';
+       }
+       prevScrollpos = currentScrollPos;
+     };
 });
