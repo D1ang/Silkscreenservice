b36dfaad495540b71a15ac76b7b4290f6f4ebba7
Djang <django.heimgartner@gmail.com>
2020-06-16 09:06:29 +0200

    Created button for lg screen with smooth scrolling

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
-	// show alerts with animation & hide them after showing.
-	$('.alert').first().hide().slideDown(500).delay(4000).slideUp(500, function() {
-		$(this).remove();
+	/* Add smooth scrolling to all links, 
+     while keeping cross-browser compatibility.
+     https://www.w3schools.com/howto/howto_css_smooth_scroll.asp */
+	$('a').on('click', function(event) {
+		if (this.hash !== '') {
+			event.preventDefault();
+
+			let hash = this.hash;
+
+			$('html, body').animate(
+				{
+					scrollTop: $(hash).offset().top
+				},
+				800,
+				function() {
+					window.location.hash = hash;
+				}
+			);
+		}
 	});
 
-	/* When the user scrolls down, hide the navbar.
-     When the user scrolls up, show the navbar.
+	/* When the user scrolls down, hide the navbar and
+     when the user scrolls up, show the navbar.
      https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp */
 	let prevScrollpos = window.pageYOffset;
 	window.onscroll = function() {
@@ -25,6 +41,11 @@ $(document).ready(function() {
 		$(element).addClass('form-control');
 	});
 
+	// show alerts with animation & hide them after showing.
+	$('.alert').first().hide().slideDown(500).delay(4000).slideUp(500, function() {
+		$(this).remove();
+	});
+
 	//DataTable settings
 	let table = $('#dataTable').DataTable({
 		lengthChange: false,
@@ -32,6 +53,6 @@ $(document).ready(function() {
 		info: false,
 		paging: false,
 		bSort: false,
-		responsive: true,
+		responsive: true
 	});
 });
