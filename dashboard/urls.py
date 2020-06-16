82338b45efeca55a8ef393e597e74f8ec5f8ac8d
Djang <django.heimgartner@gmail.com>
2020-06-16 06:50:54 +0200

    Created an order detail page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, orderdetails
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+    path('orderdetails/<str:pk_order>/', orderdetails, name='orderdetails'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from orders.models import Order
 
 
 def dashboard(request):
@@ -7,7 +8,7 @@ def dashboard(request):
     A view that displays the dashboard
     for the customer & paginate the order list.
     """
-    order_list = request.user.order_set.all()
+    order_list = request.user.order_set.all().order_by('-date')
     paginator = Paginator(order_list, 6)
     page_number = request.GET.get('page')
     page_object = paginator.get_page(page_number)
@@ -24,3 +25,16 @@ def dashboard(request):
     }
 
     return render(request, 'dashboard/customer.html', context)
+
+
+def orderdetails(request, pk_order):
+    """
+    Updating an exciting order.
+    """
+    order = Order.objects.get(id=pk_order)
+
+    context = {
+        'order': order,
+    }
+
+    return render(request, 'dashboard/orderdetails.html', context)
