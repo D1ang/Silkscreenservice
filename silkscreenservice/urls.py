0a4b7ad2794179c8ff56aa82fe4a405e695cb600
Djang <django.heimgartner@gmail.com>
2020-06-26 12:18:00 +0200

    Bugfix: redirect to login when an unauthenticated user added a service

--- silkscreenservice/urls.py
+++ silkscreenservice/urls.py
@@ -24,7 +24,7 @@ admin.site.index_title = "Welcome to the Silkscreenservice Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
 
     path('', include('home.urls', namespace='home')),
