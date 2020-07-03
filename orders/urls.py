e874c94530b5f71d01c66194c288729b4c3c43f4
Djang <django.heimgartner@gmail.com>
2020-07-03 16:49:34 +0200

    Moved the urls from orders to the new checkout app

--- orders/urls.py
+++ orders/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import (
     ItemListView,
     OrderSummaryView,
-    CheckoutView,
-    PaymentView,
     add_to_cart,
     remove_from_cart
 )
@@ -13,8 +11,6 @@ app_name = 'orders'
 urlpatterns = [
     path('services/', ItemListView.as_view(), name='services'),
     path('cart/', OrderSummaryView.as_view(), name='cart'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('payment/', PaymentView.as_view(), name='payment'),
 
     path('add_to_cart/<slug>/', add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<slug>/', remove_from_cart, name='remove_from_cart')
