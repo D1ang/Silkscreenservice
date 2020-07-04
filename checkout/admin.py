b3662401ec0fc7a5a9bd32ccd2f1809d9ae086d6
Djang <django.heimgartner@gmail.com>
2020-07-04 10:53:49 +0200

    Improved the admin payment & billing address view

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,23 +4,29 @@ from .models import Payment, BillingAddress
 
 class BillingAddressAdmin(admin.ModelAdmin):
     list_display = (
-        'user',
+        'company_name',
         'first_name',
         'last_name',
         'street_address',
-        'address_line_2',
         'city',
         'region',
         'postal',
         'country'
     )
     search_fields = (
-        'user',
+        'company_name',
         'street_address',
         'city',
         'country'
     )
 
 
-admin.site.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'amount'
+    )
+
+
+admin.site.register(Payment, PaymentAdmin)
 admin.site.register(BillingAddress, BillingAddressAdmin)
