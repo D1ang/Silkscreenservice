dde094015465dca99d2d776e24a0ba1b0e66319d
Djang <django.heimgartner@gmail.com>
2020-07-06 15:39:10 +0200

    Beautfied the cripsy.js comment

--- static/js/crispy.js
+++ static/js/crispy.js
@@ -1,7 +1,7 @@
+/* Replace the "Currently" artwork update field placeholder.
+   As standard Crispy-forms file field is not repsonsive on smaller screens
+   The placeholder text overflows the field, this will fix that problem */
 $(document).ready(function() {
-	/* Replace the "Currently" artwork update field placeholder.
-     As standard Crispy-forms file field is not repsonsive on smaller screens
-     The placeholder text overflows the field, this will fix that problem */
 	let currentArtworkLink = document.getElementById('div_id_artwork').getElementsByClassName('d-flex')[0];
 	const replaceArtworkLink = '<span><i class="fas fa-file-alt"></i> Artwork uploaded</span>';
 
