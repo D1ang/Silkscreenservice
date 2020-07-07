4ccde471dd78fe8d1b8efaabe6eaf6d26c0465d6
Djang <django.heimgartner@gmail.com>
2020-07-07 14:22:44 +0200

    Changed the max character length of the address fields to 50 characters

--- accounts/models.py
+++ accounts/models.py
@@ -10,16 +10,16 @@ class Customer(models.Model):
     A customer can only have 1 user & user only 1 customer
     """
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    company_name = models.CharField(max_length=25)
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    street_address = models.CharField(max_length=25)
-    address_line_2 = models.CharField(max_length=25, blank=True, null=True)
-    city = models.CharField(max_length=25)
-    region = models.CharField(max_length=25, blank=True, null=True)
+    company_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    street_address = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50)
+    region = models.CharField(max_length=50, blank=True, null=True)
     postal = models.CharField(max_length=6)
     country = CountryField(multiple=False)
-    email = models.CharField(max_length=25)
+    email = models.CharField(max_length=50)
     phone = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
