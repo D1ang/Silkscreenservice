36f7544198f305942631d6c37a79ad9fda4b84b7
Djang <django.heimgartner@gmail.com>
2020-07-10 13:00:22 +0200

    Changed the Datefilter placeholder,  as it was too unclear

--- accounts/filters.py
+++ accounts/filters.py
@@ -22,7 +22,7 @@ class OrderFilter(filters.FilterSet):
         field_name='date',
         label='Date',
         lookup_expr='gte',
-        widget=TextInput(attrs={'placeholder': 'Date'})
+        widget=TextInput(attrs={'placeholder': 'Date from to today'})
     )
 
     class Meta:
